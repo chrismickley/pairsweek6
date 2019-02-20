6d9834790b27ff9a118d9314862654e6e5ff652b
Chris Mickley <chris.mickley@gmail.com>
2019-02-20 11:30:06 -0500

    save work

--- m2-06_Database_Connectivity_DAO/src/test/java/com/techelevator/projects/view/JDBCProjectDAOTest.java
+++ m2-06_Database_Connectivity_DAO/src/test/java/com/techelevator/projects/view/JDBCProjectDAOTest.java
@@ -2,13 +2,42 @@ package com.techelevator.projects.view;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-public class JDBCProjectDAOTest {
+import com.techelevator.projects.model.Project;
+import com.techelevator.projects.model.jdbc.JDBCProjectDAO;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class JDBCProjectDAOTest extends DAOIntegrationTest{
+	JDBCProjectDAO testing;
+	JdbcTemplate jdbcTemplate;
+	
+	
+
+	@Before
+	public void setup() {
+		testing = new JDBCProjectDAO(getDataSource());
+//		jdbcTemplate = new JdbcTemplate(getDataSource());
+
 	}
+	
+	@Test
+	public void getAllActiveProjectsTest() {
+		List <Project> project = testing.getAllActiveProjects();
+		assertEquals(6, project.size());
+	}
+	
+//	@Test
+//	public void removeEmployeeFromProjectTest() {
+//		fail("Not yet implemented");
+//	}
 
+//	@Test
+//	public void addEmployeeToProject(Long projectId, Long employeeId) {
+//	fail("Not yet implemented");
+//	}
+	
 }
